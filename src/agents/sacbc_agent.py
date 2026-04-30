made some fixes for baseline so its a little more suited for the KL stuff

src/agents/sacbc_agent.py
@@ -94,8 +94,9 @@ class SACBCAgent(nn.Module):
         q = qs.min(dim=0).values
         q_loss = -q.mean()
 
-        mses = nn.functional.mse_loss(actor_actions, actions)
-        bc_loss = self.alpha * (1/actions.shape[1]) * mses
+        # mses = nn.functional.mse_loss(actor_actions, actions)
+        # bc_loss = self.alpha * (1/actions.shape[1]) * mses
+        bc_loss = -self.alpha * actor_dists.log_prob(actions).mean() #replaced so that we are actually approximating reverse KL with this constraint
 
         log_probs = actor_dists.log_prob(actor_actions).mean()
         entropy_loss = self.beta.forward() * log_probs